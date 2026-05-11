fb4fe1c40464bde4befc2669cf4c642579bba84b
Pijar Adiluhung <you@example.com>
2026-05-11 19:54:55 +0700

    added tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
+  <h1 class="text-3xl font-bold text-center">You did it!</h1>
+  <p class="text-center mt-4">
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
